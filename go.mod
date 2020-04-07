762bfdced28f2ddb12272c31fd3bbd130151c30f
qiuxuwei <qiuxuwei@bigo.sg>
2020-04-07 10:23:32 +0800

    +Defaultip

--- go.mod
+++ go.mod
@@ -1,7 +1,5 @@
 module github.com/weipatty/gomyenv
 
-require (
-	github.com/Shopify/sarama v1.24.1
-)
+require github.com/Shopify/sarama v1.24.1
 
 go 1.13
--- utilfun.go
+++ utilfun.go
@@ -1,6 +1,8 @@
 package gomyenv
 
 import (
+	"errors"
+	"net"
 	"reflect"
 	"strings"
 	"time"
@@ -47,4 +49,50 @@ func CreateAnyTypeSlice(slice interface{}) ([]interface{}, bool) {
 		out[i] = val.Index(i).Interface()
 	}
 	return out, true
+}
+
+// DefaultIP get a default non local ip, err is not nil, ip return 127.0.0.1
+func DefaultIP() (ip string, err error) {
+	ip = "127.0.0.1"
+
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return
+	}
+
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			continue
+		}
+
+		for _, addr := range addrs {
+			if ipStr := getAddrDefaultIP(addr); len(ipStr) > 0 {
+				return ipStr, nil
+			}
+		}
+	}
+
+	err = errors.New("no ip found")
+	return
+}
+
+func getAddrDefaultIP(addr net.Addr) string {
+	var ip net.IP
+	switch v := addr.(type) {
+	case *net.IPNet:
+		ip = v.IP
+	case *net.IPAddr:
+		ip = v.IP
+	}
+	if ip.IsUnspecified() || ip.IsLoopback() {
+		return ""
+	}
+
+	ip = ip.To4()
+	if ip == nil {
+		return ""
+	}
+
+	return ip.String()
 }
